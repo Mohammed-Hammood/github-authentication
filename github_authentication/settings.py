70cb85da701e2f423c6395e7ecbb48f094cac589
Mohammed <iraqiProfessionalDesigner@gmail.com>
2022-09-11 14:59:30 +0700

    static files collected

--- github_authentication/settings.py
+++ github_authentication/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static'
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.github.GithubOAuth2',
